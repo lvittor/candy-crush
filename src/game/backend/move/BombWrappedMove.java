00f5ebae6bc4b324606f446414b365d7005100f2
lvittor <lvittor@itba.edu.ar>
2020-12-05 17:03:30 -0300

    Delete duplicated code and add comments

--- src/game/backend/move/BombWrappedMove.java
+++ src/game/backend/move/BombWrappedMove.java
@@ -1,10 +1,8 @@
 package game.backend.move;
 
 import game.backend.Grid;
-import game.backend.element.Bomb;
-import game.backend.element.Candy;
 
-public class BombWrappedMove extends Move {
+public class BombWrappedMove extends BombMove {
 
 	public BombWrappedMove(Grid grid) {
 		super(grid);	
@@ -12,23 +10,19 @@ public class BombWrappedMove extends Move {
 	
 	@Override
 	public void removeElements() {
-		Candy candy = (Candy) (get(i1, j1) instanceof Bomb ? get(i2, j2) : get(i1, j1));
-		clearContent(i1, j1);
-		clearContent(i2, j2);
-		for(int i = 0; i < Grid.SIZE; i++) {
-			for(int j = 0; j < Grid.SIZE; j++) {
-				if (candy.equals(get(i, j))) {
-					clearContent(i, j);
-				}
-			}
-		}
-		for(int i = -1; i < 2; i++) {
-			for(int j = -1; j < 2; j++) {
-				if (i1 + i >= 0 && i1 + i < Grid.SIZE && j1 + j >= 0 && j1 + j < Grid.SIZE) {
-					clearContent(i1 + i, j1 + j);
-				}
-			}
-		}
+
+		/*
+		* Como un BombWrappedMove tiene casi la misma implementacion y funcionamiento de un BombMove, hicimos que extienda de esta.
+		* */
+
+		super.removeElements();
+
+		//Los dos fors siguientes podrían estar modularizados en una unica funcion
+		clearClose(i1, j1);
+		clearClose(i2, j2);
+	}
+
+	private void clearClose(int i2, int j2) {
 		for(int i = -1; i < 2; i++) {
 			for(int j = -1; j < 2; j++) {
 				if (i2 + i >= 0 && i2 + i < Grid.SIZE && j2 + j >= 0 && j2 + j < Grid.SIZE) {
@@ -37,5 +31,4 @@ public class BombWrappedMove extends Move {
 			}
 		}
 	}
-
 }
--- src/game/frontend/ImageManager.java
+++ src/game/frontend/ImageManager.java
@@ -27,21 +27,27 @@ public class ImageManager {
 		images.put(new Nothing().getKey(), new Image(IMAGE_PATH + "nothing.png"));
 		images.put(new Bomb().getKey(),  new Image(IMAGE_PATH + "bomb.png"));
 		images.put(new Wall().getKey(),  new Image(IMAGE_PATH + "wall.png"));
+		/*
+		 * Econtramos que para setear las imagenes con sus colores se recorrian estos 4 veces pero se podia hacer con un
+		 * solo recorrido.
+		 * */
 		for (CandyColor cc: CandyColor.values()) {
+			// Agregamos los Candy con sus imagenes
 			images.put(new Candy(cc).getFullKey(),   new Image(IMAGE_PATH + cc.toString().toLowerCase() + "Candy.png"));
-		}
-		for (CandyColor cc: CandyColor.values()) {
+
+			// Agregamos los WrappedCandy con sus imagenes
 			wc.setColor(cc);
 			images.put(wc.getFullKey(),  new Image(IMAGE_PATH + cc.toString().toLowerCase() + "Wrapped.png"));
-		}
-		for (CandyColor cc: CandyColor.values()) {
+
+			// Agregamos los VerticalStripedCandy con sus imagenes
 			vc.setColor(cc);
 			images.put(vc.getFullKey(),  new Image(IMAGE_PATH + cc.toString().toLowerCase() + "VStripped.png"));
-		}
-		for (CandyColor cc: CandyColor.values()) {
+
+			// Agregamos los HorizontalStripedCandy con sus imagenes
 			hc.setColor(cc);
 			images.put(hc.getFullKey(),  new Image(IMAGE_PATH + cc.toString().toLowerCase() + "HStripped.png"));
 		}
+
 	}
 
 	public Image getImage(Element e) {
