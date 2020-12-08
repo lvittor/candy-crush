ba9d61900fa523268dd99327994d76007cfc83cd
jero_brave <jbrave@itba.edu.ar>
2020-12-08 20:59:32 -0300

    Modified fram gap.

--- src/game/frontend/CandyFrame.java
+++ src/game/frontend/CandyFrame.java
@@ -60,7 +60,7 @@ public class CandyFrame extends VBox {
 			@Override
 			public void gridUpdated() {
 				Timeline timeLine = new Timeline();
-				Duration frameGap = Duration.millis(5);
+				Duration frameGap = Duration.millis(100);
 				Duration frameTime = Duration.ZERO;
 				for (int i = game().getSize() - 1; i >= 0; i--) {
 					for (int j = game().getSize() - 1; j >= 0; j--) {
